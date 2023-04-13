514dd1dc7450919184b60b2406e4f913212b56bd
Isabel Oliveira <isabel.01oa@outlook.com>
2023-04-13 17:40:30 -0300

    deleted try-catch, added do-while and changed nextInt to nextLong and managed to solve the problem

--- src/LargestPrimeFactor.java
+++ src/LargestPrimeFactor.java
@@ -1,29 +1,20 @@
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class LargestPrimeFactor {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        NumberFormat nFormat = NumberFormat.getNumberInstance(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n;
-
-        try {
-            n = sc.nextInt();
-        }
-        catch (InputMismatchException err) {
-            n = nFormat.parse(sc.nextLine()).intValue();
-        }
+        long n = sc.nextLong();
 
         for(int i = 2; n != 1; i++) {
             if(((i == 2 || i == 3 || i == 5) || (i % 2 != 0 && i % 3 != 0 && i % 5 != 0)) && n % i == 0) {
-                n /= i;
-                System.out.println(i);
+                do {
+                    n /= i;
+                    System.out.println(i);
+                } while (n % i == 0);
             }
         }
 
